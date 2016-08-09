9d68fa5a88e51be80a410da0f1e985fb8327e33a
Mario Schreiner <mario.schreiner@uni-konstanz.de>
2016-08-09 20:23:33 +0100

    Added missing Config.m

---
+++ PokeAlert/Config.m
@@ -0,0 +1,11 @@
+//
+//  Config.m
+//  PokeAlert
+//
+//  Created by Mario Schreiner on 30/07/16.
+//  Copyright © 2016 Mario Schreiner. All rights reserved.
+//
+
+#import "Config.h"
+
+NSString *const SERVER_ADDRESS = @"http://SERVER_ADDRES:SERVER_PORT";
